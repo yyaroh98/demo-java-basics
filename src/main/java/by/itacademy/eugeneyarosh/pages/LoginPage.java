93cac3ac57fe0c4697b578dcd67c97704887d39e
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-20 20:43:25 +0300

    file LoginPage add method open()

--- src/main/java/by/itacademy/eugeneyarosh/pages/LoginPage.java
+++ src/main/java/by/itacademy/eugeneyarosh/pages/LoginPage.java
@@ -1,4 +1,21 @@
 package by.itacademy.eugeneyarosh.pages;
 
-public class LoginPage {
+public class LoginPage extends BasePage {
+
+    protected String LOGIN_PAGE = getBASE_URL() + "/форма логина";
+    String title = "Форма логина";
+
+    public String getLOGIN_PAGE() {
+        return LOGIN_PAGE;
+    }
+
+    public void setLOGIN_PAGE(String LOGIN_PAGE) {
+        this.LOGIN_PAGE = LOGIN_PAGE;
+    }
+
+    public void open() {
+        System.out.println("Пользователь открыл страницу " + getLOGIN_PAGE());
+    }
+
+
 }
