f252e53e8d1197dcf0e387f68d4edecc1320cd5b
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-20 20:32:34 +0300

    file HomePage add method getTitle()

--- src/main/java/by/itacademy/eugeneyarosh/pages/HomePage.java
+++ src/main/java/by/itacademy/eugeneyarosh/pages/HomePage.java
@@ -6,9 +6,22 @@ public class HomePage extends BasePage {
 
     protected String title = "Домашняя страница";
 
+    public String getHOME_URL() {
+        return HOME_URL;
+    }
+
+    public void setHOME_URL(String HOME_URL) {
+        this.HOME_URL = HOME_URL;
+    }
 
     public void open() {
-        System.out.println("Пользователь открыл страницу " + HOME_URL);
-
+        System.out.println("Пользователь открыл страницу " + getHOME_URL());
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+
 }
