f4ccf1bc6ccca8242b344d9e0b982ed4eb74bafb
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-20 20:48:55 +0300

    add file Main

---
+++ src/main/java/by/itacademy/eugeneyarosh/pages/Main.java
@@ -0,0 +1,15 @@
+package by.itacademy.eugeneyarosh.pages;
+
+public class Main {
+    public static void main(String[] args) {
+        HomePage homePage = new HomePage();
+        homePage.open();
+        System.out.println(homePage.getTitle());
+        homePage.clickLogin();
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.open();
+        System.out.println(loginPage.getTitle());
+
+    }
+}
