b8ae89c78a696625fe66b463c473587e8c4a68c0
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-21 22:42:45 +0300

    refactor file HomePage. Remove excessive code

--- src/main/java/by/itacademy/eugeneyarosh/pages/HomePage.java
+++ src/main/java/by/itacademy/eugeneyarosh/pages/HomePage.java
@@ -2,20 +2,8 @@ package by.itacademy.eugeneyarosh.pages;
 
 public class HomePage extends BasePage {
 
-    protected String HOME_URL = getBASE_URL() + "/домашняя страница";
+    protected String HOME_URL = BASE_URL + "/домашняя страница";
     String title = "Домашняя страница";
-
-    public String getHOME_URL() {
-        return HOME_URL;
-    }
-
-    public void setHOME_URL(String HOME_URL) {
-        this.HOME_URL = HOME_URL;
-    }
-
-    public void open() {
-        System.out.println("Пользователь открыл страницу " + getHOME_URL());
-    }
 
     public String getTitle() {
         return title;
@@ -23,6 +11,10 @@ public class HomePage extends BasePage {
 
     public void clickLogin() {
         System.out.println("Пользователь кликнул по кнопке Войти");
+    }
+
+    public void open() {
+        System.out.println("Пользователь открыл страницу " + HOME_URL);
     }
 }
 
