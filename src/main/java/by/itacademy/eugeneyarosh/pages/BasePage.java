84c84f1b20c8a08782c58099a7b3d7e8be9fba85
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-21 22:38:25 +0300

    refactor file BasePage. Remove excessive code

--- src/main/java/by/itacademy/eugeneyarosh/pages/BasePage.java
+++ src/main/java/by/itacademy/eugeneyarosh/pages/BasePage.java
@@ -4,15 +4,7 @@ public class BasePage {
 
     protected String BASE_URL = "BASE URL";
 
-    public String getBASE_URL() {
-        return BASE_URL;
-    }
-
-    public void setBASE_URL(String BASE_URL) {
-        this.BASE_URL = BASE_URL;
-    }
-
     public void open() {
-        System.out.println("Пользователь открыл страницу " + getBASE_URL());
+        System.out.println("Пользователь открыл страницу " + BASE_URL);
     }
 }
